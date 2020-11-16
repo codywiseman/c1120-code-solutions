329d31925b52cf19f4345de3e8d15d2b73dcf273
Cody Wiseman <cwiseman09@yahoo.com>
2020-11-16 15:24:17 -0800

    Completed javascript operators and expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+
+var width = 10;
+var height = 5;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('type of area:', typeof area);
+
+var bill = 60;
+var payment = 100;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 96;
+var midterm = 99;
+var final = 93;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('type of grade:', typeof grade);
+
+var firstName = 'Cody '
+var lastName = 'Wiseman'
+var fullName = firstName + lastName;
+
+console.log('value of fullname:', fullName);
+console.log('type of fullname:', typeof fullName);
+
+var pH = 3;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 300;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('type of motto:', typeof motto);
