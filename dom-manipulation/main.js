c0464af1d7e90508ccca66f52d91b666edff2aac
Cody Wiseman <cwiseman09@yahoo.com>
2020-11-23 18:04:32 -0800

    completed dom manipulation

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,13 +1,29 @@
-var storeClickCount;
+
+var totalClickCount = 0;
+
 var $hotButton = document.querySelector('.hot-button');
+
 var $clickCount = document.querySelector('.click-count')
 
-function clickCountHandler(event) {
-  var i = 0;
-  var clickCount = i++
-  $clickCount.textContent = 'Click Count: ' + clickCount;
 
 
-}
-
-$hotButton.addEventListener('click', clickCountHandler);
+$hotButton.addEventListener('click', function(event) {
+  totalClickCount++;
+  console.log(totalClickCount);
+  var temperature;
+  if(totalClickCount < 4) {
+    temperature = 'cold';
+  } else if (totalClickCount < 7) {
+    temperature = 'cool';
+  } else if (totalClickCount < 10) {
+    temperature = 'tepid';
+  } else if (totalClickCount < 13) {
+    temperature = 'warm';
+  } else if (totalClickCount < 16) {
+    temperature = 'hot';
+  } else {
+    temperature = 'nuclear';
+  }
+  $clickCount.textContent = 'Count: ' + totalClickCount;
+  $hotButton.className = 'hot-button ' +  temperature;
+} );
