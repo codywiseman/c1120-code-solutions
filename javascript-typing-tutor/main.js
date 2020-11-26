16d6e06fbdc8ee8d2e75875c71abb59b5a7feaf6
Cody Wiseman <cwiseman09@yahoo.com>
2020-11-26 12:30:24 -0800

    finished typing tutor, started accuracy counter

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,10 +1,24 @@
 
 var $spanElements = document.querySelectorAll('span')
 
-document.addEventListener('keydown', function(event) => {
-  if (event. ==='71') {
-    $spanElements.item(0).className = '.green';
-    $spanElements.item(1).className = '.underline';
+var $accuracyCounter = document.querySelector('p')
 
+var missedLetter = 30
+
+var typeAccuracy = ((missedLetter / 30 )* 100)
+
+var i = 0
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === ($spanElements.item(i).textContent) && ($spanElements.item(i).className === 'underline' || $spanElements.item(i).className === 'red')) {
+    $spanElements.item(i).className = 'green';
+    $spanElements.item(i + 1).className = 'underline';
+    i++;
+    return;
+  } else if (event.key !== $spanElements.item(i).textContent && $spanElements.item(i).className === 'underline') {
+    $spanElements.item(i).className = 'red';
+    missedLetter--;
+    $accuracyCounter.appendChild(typeAccuracy)
+    return;
   }
 })
