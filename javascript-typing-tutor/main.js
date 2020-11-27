f52bb27ec82f988dc5319f2c039c4d91887f2e12
Cody Wiseman <cwiseman09@yahoo.com>
2020-11-27 10:50:20 -0800

    complete javascript typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,22 +3,22 @@ var $spanElements = document.querySelectorAll('span')
 
 var $accuracyCounter = document.querySelector('p')
 
-var missedLetter = 30
+var missedLetter = 0;
 
-var typeAccuracy = ((missedLetter / 30 )* 100)
-
-var i = 0
+var i = 0;
 
 document.addEventListener('keydown', function(event) {
+  var typeAccuracy = Math.round(((30 - missedLetter) / 30) * 100);
   if (event.key === ($spanElements.item(i).textContent) && ($spanElements.item(i).className === 'underline' || $spanElements.item(i).className === 'red')) {
     $spanElements.item(i).className = 'green';
     $spanElements.item(i + 1).className = 'underline';
     i++;
+    $accuracyCounter.textContent = 'Typing Accuracy: ' + typeAccuracy + '%';
     return;
   } else if (event.key !== $spanElements.item(i).textContent && $spanElements.item(i).className === 'underline') {
     $spanElements.item(i).className = 'red';
-    missedLetter--;
-    $accuracyCounter.appendChild(typeAccuracy)
+    missedLetter++
+    $accuracyCounter.textContent = 'Typing Accuracy: ' + typeAccuracy + '%';
     return;
   }
 })
