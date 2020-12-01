371638ecbe4cee562f7d5073e3b7cb2f40aedbc0
Cody Wiseman <cwiseman09@yahoo.com>
2020-11-30 22:11:19 -0800

    completed object manipulation 2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,23 +1,11 @@
 /* exported defaults */
 
-function defaults(target,source) {
-  var newArr = [];
-  var finalObject = {};
-  for(var prop in source){
-    if(source[prop] <= 3) {
-      target[prop] = source[prop];
+function defaults(target, source) {
+  for(var prop in source) {
+    if(target.hasOwnProperty(prop)) {
+    //do nothing
+    } else {
+    target[prop] = source[prop];
     }
   }
-  for(var prop in target) {
-    newArr.push([prop, target[prop]]);
-  }
-  newArr.sort(function(a, b) {
-    return a[1] - b[1];
-  })
-  for(var i = 0; i < newArr.length; i++) {
-    for(var x = 0; x < 1; x++) {
-      finalObject[newArr[i][x]] = newArr[i][x + 1];
-    }
-  }
-  return finalObject;
 }
