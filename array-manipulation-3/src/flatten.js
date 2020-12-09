a3b221b50b35544b621a20c9e73edcdd77d23a96
Cody Wiseman <cwiseman09@yahoo.com>
2020-12-09 09:52:27 -0800

    started flatten solution

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,19 @@
 /* exported flatten */
+
+function flatten(array) {
+  var newArr = [];
+  for (var i = 0; i < array.length; i++) {
+    if(Array.isArray(array[i])){
+      newArr.push(array[i])
+    } else {
+    for(var x = 0; x < array[i].length; x++){
+      if(Array.isArray(array[x])) {
+        newArr.push(array[x])
+      } else {
+      newArr.push(array[x][i])
+      }
+    }
+  }
+  return newArr;
+}
+};
