81c81bb9ef0ccbbe15e615470a14b3be22a763e9
Cody Wiseman <cwiseman09@yahoo.com>
2020-12-09 16:52:52 -0800

    flatten

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -2,18 +2,14 @@
 
 function flatten(array) {
   var newArr = [];
-  for (var i = 0; i < array.length; i++) {
-    if(Array.isArray(array[i])){
+  for(var i = 0; i < array.length; i++) {
+    if(!Array.isArray(array[i])) {
       newArr.push(array[i])
-    } else {
-    for(var x = 0; x < array[i].length; x++){
-      if(Array.isArray(array[x])) {
-        newArr.push(array[x])
-      } else {
-      newArr.push(array[x][i])
-      }
-    }
+    } else
   }
   return newArr;
-}
 };
+
+
+
+// [false, [true, [false]], [true]]
