329111c4425c8ed595c56b32cc510cf4b7531989
Cody Wiseman <cwiseman09@yahoo.com>
2021-01-05 21:21:36 +0000

    completed node-notes-cli

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -7,39 +7,33 @@ const expression = process.argv[2];
 switch(expression) {
   case 'read':
     for(let note in data.notes) {
-      console.log(note + ':', data.notes[note])
+      console.log(`${note}: ${data.notes[note]}`)
     }
     break;
   case 'create':
-    let nextNote = 1;
-    for (let note in data.notes) {
-     nextNote = parseFloat(note) + 1;
-    }
-    data.notes[nextNote] = process.argv[3];
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1)
-      }
-    })
+    data.notes[data.nextId] = process.argv[3];
+    break;
   case 'delete':
     const deleteNote = process.argv[3];
     const {[deleteNote]: remove, ...rest} = data.notes;
     data.notes = rest;
-    fs.writeFile('data.json', JSON.stringify(data , null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1)
-      }
-    })
+    break;
   case 'update':
     const noteUpdateNumber = process.argv[3];
     const updatedNote = process.argv[4];
     data.notes[noteUpdateNumber] = updatedNote;
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1)
-      }
-    })
+    break;
+  default:
+    console.log(`${expression} is not a command`)
 }
+
+for (let note in data.notes) {
+  data.nextId = parseFloat(note) + 1;
+}
+
+fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1)
+  }
+})
