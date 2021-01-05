4125d04292a69c9a16d6715bf1b0dc4fcd08b654
Cody Wiseman <cwiseman09@yahoo.com>
2021-01-05 19:19:08 +0000

    user can add a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -9,4 +9,17 @@ switch(expression) {
     for(let note in data.notes) {
       console.log(note + ':', data.notes[note])
     }
+    break;
+  case 'create':
+    let nextNote = 1;
+    for (let note in data.notes) {
+     nextNote = parseFloat(note) + 1;
+    }
+    data.notes[nextNote] = process.argv[3];
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1)
+      }
+    })
 }
