b69311ec1c4768f86884aaee26e1ff594069798f
Cody Wiseman <cwiseman09@yahoo.com>
2021-01-08 00:09:40 +0000

    clients can GET a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -11,6 +11,17 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(dataArray);
 })
 
+app.get('/api/notes/:id', (req, res) => {
+  const getId = parseInt(req.params.id);
+  if(getId < 0 || isNaN(getId)) {
+    res.status(400).json({error: 'id must be a positive integer'});
+  } else if (data.notes[getId]){
+    res.status(200).json(data.notes[getId]);
+  } else {
+    res.status(404).json({error: `cannot find node with id ${getId}`});
+  }
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 })
