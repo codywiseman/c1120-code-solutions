1d2e9905820d1af76f62644e7eaaf8ee43c1d75d
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-26 10:57:22 -0700

    node practce

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.writeFile('random.txt', Math.random(), (err) => {
+fs.writeFile('random.txt', Math.random().toString(), (err) => {
   if (err) {
     console.error(err);
   }
